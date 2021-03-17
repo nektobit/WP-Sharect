8499c685ae73fffc8e52c2cdb390fe7b8dd2372c
Dmitrii Yakimchuk <nektobit@mail.ru>
2021-03-17 20:09:01 +1000

    fix(init): add hooks for initialization

--- assets/init.js
+++
@@ -1,1 +0,0 @@
-Sharect.init();
--- sharect.php
+++ sharect.php
@@ -1,27 +1,42 @@
 <?php
 /*
-* Plugin Name: Sharect - share text selections
+* Plugin Name: WP Sharect
 * Plugin URI: https://nektobit.ru
-* Description: A lightweight WordPress plugin to let users share their text selections to social networks. Based on https://estevanmaito.github.io/sharect/
+* Description: A lightweight WordPress plugin to let users share their text selections to social networks.
 * Version: 1.0.0
 * Author: Dmitrii Nektobit
 * Author URI: https://nektobit.ru
-* Text Domain: sharect
+* Text Domain: wpsharect
 *
 * Requires at least: 5.0
 * Licence: MIT
 */
 
-add_action('wp_enqueue_scripts', 'add_and_init');
+add_action('wp_enqueue_scripts', 'wpsharect_add');
+add_action('wp_footer', 'wpsharect_init');
 
-function add_and_init()
+function wpsharect_add()
 {
     wp_enqueue_script('sharect', plugins_url('/assets/sharect.js', __FILE__), null, '1.0.0');
-    wp_enqueue_script(
-        'sharect-init',
-        plugins_url('/assets/init.js', __FILE__),
-        array('sharect'),
-        '1.0.0',
-        true
-    );
+}
+
+function wpsharect_config() {
+    $config = "";
+    return apply_filters( 'wpsharect_config_filter', $config );
+}
+
+function wpsharect_appendCustomShareButtons() {
+    $config = "";
+    return apply_filters( 'wpsharect_appendCustomShareButtons_filter', $config );
+}
+
+function wpsharect_init() {
+    ?>
+        <script type="text/javascript">
+            Sharect
+            .config({<?= wpsharect_config() ?>})
+            .appendCustomShareButtons([<?= wpsharect_appendCustomShareButtons() ?>])
+            .init();
+        </script>
+    <?php
 }
